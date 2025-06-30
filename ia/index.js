fdba4ab8245e46a7f8cf771f3d0fb55e835e883d
tapejaralira <tapejaralira@gmail.com>
2025-06-29 21:11:58 -0400

    antes de atualizar tudo

--- ia/index.js
+++
@@ -1,9 +0,0 @@
-// Índice central para todos os módulos de IA
-
-const GeradorFalasIA = require('./geradorFalasIA');
-const IntegracaoIAExtendida = require('./integracaoIAExtendida');
-
-module.exports = {
-    GeradorFalasIA,
-    IntegracaoIAExtendida
-};
--- tts/index.js
+++
@@ -1,9 +0,0 @@
-// Índice central para todos os módulos de TTS
-
-const IntegradorElevenLabs = require('./integradorElevenLabs');
-// Adicione outros TTS aqui se necessário
-
-module.exports = {
-    IntegradorElevenLabs,
-    // outros TTS...
-};
